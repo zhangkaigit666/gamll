868f855d3c30d004a96ded0153caad330a88f830
zhangkai <zhang@qq.com>
2019-10-29 20:20:58 +0800

    admin vew 时修改

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.atguigu.gmall.pms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author zhangkai
+ * @create 2019-10-29 18:04
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+        //禁用掉csrf
+        http.csrf().disable();
+    }
+
+}
--- gmall-wms/src/main/java/com/atguigu/gmall/wms/GmallWmsApplication.java
+++ gmall-wms/src/main/java/com/atguigu/gmall/wms/GmallWmsApplication.java
@@ -1,5 +1,6 @@
 package com.atguigu.gmall.wms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,9 +9,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableSwagger2
+@MapperScan(basePackages = "com.atguigu.gmall.wms.dao")
 public class GmallWmsApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(GmallWmsApplication.class, args);
     }
 
